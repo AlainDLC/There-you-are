e72acab8c59ac23cea95c8dc73b84fe28a121aa6
AlainDLC <jd_alain@hotmail.com>
2024-10-14 15:38:27 +0200

    removde node moduler

--- script.js
+++ script.js
@@ -16,7 +16,18 @@ if (navigator.geolocation) {
     position => {
       const { latitude } = position.coords;
       const { longitude } = position.coords;
-      console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+
+      const map = L.map('map').setView([51.505, -0.09], 13);
+
+      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker([51.5, -0.09])
+        .addTo(map)
+        .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+        .openPopup();
     },
     () => {
       alert('Could no get your position');
